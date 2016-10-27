d0f3fa00d233872d5316fd4303b1ac4689386f3f
Charles McClendon <charles.mcclendon@gmail.com>
2016-10-27 00:13:59 -0400

    fixed test.py so it uses the new API. The new API can now run the whole test.py from before, with 'cleaner' code.

--- stann.py
+++ stann.py
@@ -1,3 +1,4 @@
+import sys
 import stann_backend
 
 DATA_TYPE_USHORT    = 1
@@ -12,6 +13,7 @@ DATA_TYPE_DOUBLE    = 8
 POINT_TYPE_DPOINT = 1
 
 DIM = 3
+MAX_DIM = 5
 DATA_TYPE = DATA_TYPE_INT
 POINT_TYPE = POINT_TYPE_DPOINT
 
@@ -30,30 +32,55 @@ DATA_TYPES = {
     DATA_TYPE_DOUBLE: 	"Double",
 }
 
-def getStannFunctionName(prefix, hasPoint=False):
+def getStannName(prefix, hasPoint=False, dim=None, dataType=None):
+    dim = dim if dim else DIM
+    dataType = dataType if dataType else DATA_TYPE
     if hasPoint:
         return "{0}_{1}_{2}d_{3}".format(
 				prefix
 				, POINT_TYPES[POINT_TYPE]
-                                , DIM
-				, DATA_TYPES[DATA_TYPE])
+                                , dim
+				, DATA_TYPES[dataType])
     else:
         return "{0}_{1}d_{2}".format(
                                 prefix
-                                , DIM
-                                , DATA_TYPES[DATA_TYPE])
+                                , dim
+                                , DATA_TYPES[dataType])
 
-def getStannFunction(prefix, hasPoint=False):
-    funcName = getStannFunctionName(prefix, hasPoint)
+def getStannFunctionName(prefix, hasPoint=False, dim=None, dataType=None):
+    return getStannName(prefix, hasPoint, dim, dataType)
+
+def getStannFunction(prefix, hasPoint=False, dim=None, dataType=None):
+    funcName = getStannFunctionName(prefix, hasPoint, dim, dataType)
     return getattr(stann_backend, funcName)
 
+def getStannClassName(prefix, hasPoint=False, dim=None, dataType=None):
+    return getStannName(prefix, hasPoint, dim, dataType)
+
+def getStannClass(prefix, hasPoint=False, dim=None, dataType=None):
+    className = getStannClassName(prefix, hasPoint, dim, dataType)
+    return getattr(stann_backend, className)
 
 def createPointVector():
-    func = getStannFunction("Vector", True)
+    func = getStannFunction("Vector", hasPoint=True)
     return func() 
 
+def createDataVector(dataType=None):
+    dataType = dataType if dataType else DATA_TYPE
+    className = "{0}Vector".format(DATA_TYPES[dataType])
+    return getattr(stann_backend, className)()
+
+def createAnswerVector():
+    return createDataVector(dataType=DATA_TYPE_ULONG)
+
+def bruteNN(pointVector, n):
+    return getStannClass("bruteNN", hasPoint=True)(pointVector, n)
+
+def sfcnn(pointVector, n):
+    return getStannClass("sfcnn", hasPoint=True)(pointVector, n)
+
 def newRandomPoint(min_val, max_val):
-    func = getStannFunction("newRandomPoint", True)
+    func = getStannFunction("newRandomPoint", hasPoint=True)
     return func(min_val, max_val)
 
 def configure(dim, data_type, point_type=POINT_TYPE_DPOINT):
@@ -61,16 +88,21 @@ def configure(dim, data_type, point_type=POINT_TYPE_DPOINT):
     DATA_TYPE = data_type
     POINT_TYPE = POINT_TYPE_DPOINT
 
-#stann_backend.DPoint_3d_Int.points = patch_DPoint_3d_Int_points
+
+for dataTypeKey, dataTypeValue in DATA_TYPES.items():
+    className = "{0}Vector".format(dataTypeValue)
+    thisModule = sys.modules[__name__]
+    setattr(thisModule, className, getattr(stann_backend, className))
 
 # To patch the DPoint_DIMd_TYPE functions with a points() func
 # that returns a TYPEArray; it's a wrapper arond point_begin()
 for pointKey, pointValue in POINT_TYPES.items():
     for dataTypeKey, dataTypeValue in DATA_TYPES.items():
-        for d in range(5):
+        for d in range(MAX_DIM):
+            dim = d+1
             pointClassName = "{0}_{1}d_{2}".format(
                                             pointValue
-                                            , d+1
+                                            , dim
                                             , dataTypeValue)
             pointClass = getattr(stann_backend, pointClassName)
             arrayClassFuncName = "{0}Array_frompointer".format(dataTypeValue)
@@ -78,3 +110,30 @@ for pointKey, pointValue in POINT_TYPES.items():
             setattr(pointClass, "points"
                         , lambda self, bound_arrayClassFunc=arrayClassFunc
                             : bound_arrayClassFunc(self.point_begin()))
+
+            dimAttrName = "_{0}__DIM".format(pointClassName)
+            setattr(pointClass, "DIM", getattr(pointClass, dimAttrName))
+            setattr(pointClass, "point_set"
+                        , lambda self, 
+                            : set([self.points()[i] for i in range(self.DIM)]))
+
+            setattr(pointClass, "DATA_TYPE", dataTypeValue)
+
+            vectorClass = getStannClass("Vector", hasPoint=True, dim=dim, dataType=dataTypeKey)
+            setattr(vectorClass, "DIM", dim)
+            setattr(vectorClass, "DATA_TYPE", dataTypeValue)
+
+
+            algoNames = ["bruteNN", "sfcnn"]
+            for algoName in algoNames:
+                if dim != 3 or dataTypeValue != "Int":
+                    continue
+
+                algoClass = getStannClass(algoName, hasPoint=True, dim=dim, dataType=dataTypeKey)
+                setattr(algoClass, "DIM", dim)
+                setattr(algoClass, "DATA_TYPE", dataTypeValue)
+                setattr(algoClass, "POINT_TYPE", pointValue)
+                setattr(algoClass, "createDataVector"
+                        , lambda self, bound_dataType=dataTypeKey
+                            : createDataVector(dataType=bound_dataType))
+
--- test.py
+++ test.py
@@ -1,35 +1,33 @@
-import stann_backend
+import stann
 
 Size = 500
 k = 5
 nnMin = -1000000000
 nnMax =  1000000000
 
-data = stann.Vector_DPoint_3d_Int()
-query = stann.Vector_DPoint_3d_Int()
-bf_ans = stann.ULongVector()
-sfcnn_ans = stann.ULongVector()
-
-#data.resize(Size)
-#query.resize(Size)
+data = stann.createPointVector()
+query = stann.createPointVector()
+bf_ans = stann.createAnswerVector()
+sfcnn_ans = stann.createAnswerVector()
 
 
-for i in xrange(Size):
-	data.push_back(stann.newRandomPoint_DPoint_3d_Int(nnMin, nnMax))
-	query.push_back(stann.newRandomPoint_DPoint_3d_Int(nnMin, nnMax))
 
-bf = stann.bruteNN_DPoint_3d_Int(data[0], Size)
-sfc = stann.sfcnn_DPoint_3d_Int(data[0], Size)
+for i in range(Size):
+    data.push_back(stann.newRandomPoint(nnMin, nnMax))
+    query.push_back(stann.newRandomPoint(nnMin, nnMax))
+
+bf = stann.bruteNN(data[0], Size)
+sfc = stann.sfcnn(data[0], Size)
 
 success = True
-for i in xrange(Size):
-	bf.ksearch(query[i], k, bf_ans)
-	sfc.ksearch(query[i], k, sfcnn_ans)
+for i in range(Size):
+    bf.ksearch(query[i], k, bf_ans)
+    sfc.ksearch(query[i], k, sfcnn_ans)
 
-	for j in xrange(3):
-		if bf_ans[j] != sfcnn_ans[j]:
-			print "i={0}, bf[{1}] = {2}, sfcnn[{1}]={3}".format(i,j,bf_ans[j], sfcnn_ans[j])
-			success = False
+    for j in range(bf.DIM):
+        if bf_ans[j] != sfcnn_ans[j]:
+            print("i={0}, bf[{1}] = {2}, sfcnn[{1}]={3}".format(i,j,bf_ans[j], sfcnn_ans[j]))
+            success = False
 
 
-print success
+print(success)
