e3f4d7cb016dd6167aef7b4f1c41a2e5f06f3cfc
dfish13 <fisherduncan13@yahoo.com>
2016-12-04 20:22:13 -0500

    added sfcnn_knng

--- dot_i_generator.py
+++ dot_i_generator.py
@@ -1,7 +1,7 @@
 ctypes = [
     ('UShort', 'unsigned short'),
     ('Short', 'short'),
-    ('Uint', 'unsigned int'),
+    ('UInt', 'unsigned int'),
     ('Int', 'int'),
     ('ULong', 'unsigned long'),
     ('Long', 'long'),
@@ -11,7 +11,7 @@ ctypes = [
 
 for i in range(1,6):
     for key, value in ctypes:
-        name = "%template(bruteNN_DPoint_{}d_{})".format(i,key)
-        declaration = "::bruteNN<reviver::dpoint<{0:<14}, {1}>>;".format(value, i)
-        print("{0:<37}{1}".format(name, declaration))
+        name = "%template(sfcnn_knng_DPoint_{}d_{})".format(i,key)
+        declaration = "::sfcnn_knng<reviver::dpoint<{0:<14}, {1}>, {1}, {0:<14}>;".format(value, i)
+        print("{0:<45}{1}".format(name, declaration))
     print()
