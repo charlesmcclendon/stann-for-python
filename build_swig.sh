bec41d66ce75a12cf696d018f640905fc50ad00d
Charles McClendon <charles.mcclendon@gmail.com>
2016-12-07 22:37:28 -0500

    final minor updates

--- build_swig.sh
+++ build_swig.sh
@@ -5,3 +5,8 @@ rm _stann_backend.so stann_backend_wrap.*
 g++ -c -O3 -Wall -pedantic -ansi -Wextra -fopenmp -Wconversion -fPIC $(python3.5-config --cflags) -I./include/ stann_backend_wrap.cxx
 
 g++ -shared -fopenmp stann_backend_wrap.o $(python3.5-config --ldflags) -o _stann_backend.so
+
+mkdir -p distrib/stann
+
+cp stann.py stann_backend.py _stann_backend.so distrib/stann/
+mv distrib/stann/stann.py distrib/stann/__init__.py
